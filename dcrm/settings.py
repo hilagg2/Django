bf2e778066cea6c152b76c4567b12ede02f4fc75
hilagg2 <hilagg2@users.noreply.github.com>
2026-06-23 08:54:51 -0500

    arreglando el chicharron del csrf porque no dejaba loguear a nadie

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os # type: ignore
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f_l@&prlsuv^pld#05^#i!p=(5@ao)3rwv@22bv$bufz+xap7w'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
 
 
@@ -139,4 +144,6 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.loca.lt',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
 ]
