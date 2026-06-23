70b675cc359a2fad69c1ac6f89c0f593183d8832
hilagg2 <hilagg2@users.noreply.github.com>
2026-06-23 08:54:54 -0500

    agregando la url de update_record que faltaba pa que funcione editar usuario

--- website/urls.py
+++ website/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [ # type: ignore
     path('delete_record/<int:pk>/', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>/', views.update_record, name='update_record'),
+    path('profile/', views.profile, name='profile'),
+    path('manage_users/', views.manage_users, name='manage_users'),
+    path('admin_ranking/', views.admin_ranking, name='admin_ranking'),
+    path('admin_games/', views.admin_games, name='admin_games'),
 ]
